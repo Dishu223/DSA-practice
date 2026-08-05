9ae815d53d4062f905c24c2fb349e20f52768804
Dishu223 <divyanshurprcg@gmail.com>
2026-08-05 23:46:23 +0530

    Solved 66. Plus One

--- 02_Arrays_and_Vectors/66. Plus One.cpp
+++ 02_Arrays_and_Vectors/66. Plus One.cpp
@@ -18,3 +18,12 @@ public:
 
     }
 };
+
+/*
+ * Time Complexity: O(N) - Traversing digits array from right to left once.
+ * Space Complexity: O(1) auxiliary space (or O(N) in worst case when adding leading 1 for all 9s).
+ *
+ * Summary:
+ * Incremented the large integer represented by digits by starting from the least significant digit,
+ * setting 9s to 0 until a digit less than 9 is incremented, handling carry overflow by prepending 1.
+ */
